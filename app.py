669011c14b3b8ab0386b018a3dc8af0307661d25
datehoer <datehoer@gmail.com>
2024-10-29 21:42:17 +0800

    add more top list

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from config import *
 import time
 from fastapi.middleware.cors import CORSMiddleware
 from sendEmail import send_email
-from common import parse_zhihu_hot_list, parse_weibo_hot_search, parse_bilibili_hot, parse_douyin_hot, parse_juejin_hot, parse_shaoshupai_hot, parse_tieba_topic, parse_toutiao_hot, parse_wx_read_rank
+from common import *
 from pydantic import BaseModel
 app = FastAPI()
 app.add_middleware(
@@ -32,7 +32,46 @@ table_dict = [
     {"name": "头条热榜", "tablename": "toutiao_hot"},
     {"name": "微博热搜", "tablename": "weibo_hot_search"},
     {"name": "微信阅读排行榜", "tablename": "wx_read_rank"},
-    {"name": "知乎热榜", "tablename": "zhihu_hot_list"}
+    {"name": "知乎热榜", "tablename": "zhihu_hot_list"},
+
+    {"name": "3DM", "tablename": "3dm"},
+    {"name": "36氪", "tablename": "36kr"},
+    {"name": "52破解热榜", "tablename": "52pj"},
+    {"name": "AcFun热榜", "tablename": "acfun"},
+    {"name": "安全客安全快讯", "tablename": "anquanke"},
+    {"name": "百度热搜", "tablename": "baidu_hot_search"},
+    {"name": "白鲸出海", "tablename": "baijingchuhai"},
+    {"name": "CSDN热榜", "tablename": "csdn"},
+    {"name": "电商报最新消息", "tablename": "dianshangbao"},
+    {"name": "第一财经热榜", "tablename": "diyicaijing"},
+    {"name": "懂车帝热搜榜", "tablename": "dongchedi"},
+    {"name": "豆瓣电影排行", "tablename": "douban_movie"},
+    {"name": "FreeBuf咨询", "tablename": "freebuf"},
+    {"name": "GitHub Trending", "tablename": "github"},
+    {"name": "Google 热搜", "tablename": "google_search"},
+    {"name": "虎扑社区热帖", "tablename": "hupu"},
+    {"name": "虎嗅热文", "tablename": "huxiu"},
+    {"name": "IT之家热榜", "tablename": "ithome"},
+    {"name": "开眼", "tablename": "openeye"},
+    {"name": "看雪热门", "tablename": "kanxue"},
+    {"name": "宽带山热榜", "tablename": "kuandaishan"},
+    {"name": "PMCAFF精选", "tablename": "pmcaff"},
+    {"name": "汽车之家热帖榜", "tablename": "qichezhijia"},
+    {"name": "起点榜单", "tablename": "qidian"},
+    {"name": "水木社区热门话题", "tablename": "shuimu"},
+    {"name": "新浪热门", "tablename": "sina"},
+    {"name": "新浪体育热门", "tablename": "sina_sport"},
+    {"name": "新浪新闻热门", "tablename": "sina_news"},
+    {"name": "太平洋汽车热门", "tablename": "taipingyang"},
+    {"name": "TapTap热门", "tablename": "taptap"},
+    {"name": "腾讯新闻热点榜", "tablename": "tencent_news"},
+    {"name": "人人都是产品经理热门", "tablename": "woshipm"},
+    {"name": "雪球热门", "tablename": "xueqiu"},
+    {"name": "易车热门", "tablename": "yiche"},
+    {"name": "优设读报", "tablename": "youshedubao"},
+    {"name": "游戏葡萄文章推荐", "tablename": "youxiputao"},
+    {"name": "站酷榜单", "tablename": "zhanku"},
+    {"name": "纵横24小时畅销榜", "tablename": "zongheng"}
 ]
 backoff = ExponentialBackoff(cap=2, base=2)
 retry = Retry(backoff=backoff, retries=10)
@@ -120,12 +159,51 @@ async def get_data(item_id: str):
                     latest_record = parse_toutiao_hot(latest_record)
                 elif collection_name == "wx_read_rank":
                     latest_record = parse_wx_read_rank(latest_record)
+                elif collection_name == "acfun":
+                    latest_record = parse_acfun(latest_record)
+                elif collection_name == "anquanke":
+                    latest_record = parse_anquanke(latest_record)
+                elif collection_name == "csdn":
+                    latest_record = parse_csdn(latest_record)
+                elif collection_name == "openeye":
+                    latest_record = parse_openeye(latest_record)
+                elif collection_name == "pmcaff":
+                    latest_record = parse_pmcaff(latest_record)
+                elif collection_name == "woshipm":
+                    latest_record = parse_woshipm(latest_record)
+                elif collection_name == "xueqiu":
+                    latest_record = parse_xueqiu(latest_record)
+                elif collection_name == "yiche":
+                    latest_record = parse_yiche(latest_record)
+                elif collection_name == "youshedubao":
+                    latest_record = parse_youshedubao(latest_record)
+                elif collection_name == "youxiputao":
+                    latest_record = parse_youxiputao(latest_record)
+                elif collection_name == "zhanku":
+                    latest_record = parse_zhanku(latest_record)
+                elif collection_name == "zongheng":
+                    latest_record = parse_zongheng(latest_record)
+                elif collection_name in ["baidu_hot_search", "3dm", "36kr", "52pj", "baijingchuhai", "dianshangbao", "diyicaijing", "dongchedi", "freebuf", "github", "google_search", "hupu", "huxiu", "ithome", "kanxue", "kuandaishan", "qichezhijia", "qidian", "shuimu", "sina", "sina_sport", "sina_news", "taipingyang", "taptap"]:
+                    latest_record = parse_common(latest_record)
                 local_time = time.localtime(insert_time)
-                data.append({
-                    "name": item["name"],
-                    "data": latest_record,
-                    "insert_time": time.strftime("%Y-%m-%d %H:%M:%S", local_time)
-                })
+                if collection_name != "douban_movie":
+                    data.append({
+                        "name": item["name"],
+                        "data": latest_record,
+                        "insert_time": time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+                    })
+                else:
+                    koubei, beimei = parse_douban(latest_record)
+                    data.append({
+                        "name": "豆瓣电影一周口碑榜",
+                        "data": koubei,
+                        "insert_time": time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+                    })
+                    data.append({
+                        "name": "豆瓣电影北美票房榜",
+                        "data": beimei,
+                        "insert_time": time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+                    })
             except Exception as e:
                 print(f"Error parsing {collection_name}: {e}")
                 print(traceback.format_exc())
