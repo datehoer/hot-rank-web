525465faf589f70f71736fbb30f588d6f061a278
datehoer <datehoer@gmail.com>
2025-01-02 13:43:07 +0800

    fix Chinese Almanac

--- tools/pushSomethingTask.py
+++ tools/pushSomethingTask.py
@@ -2,9 +2,11 @@ import json
 import time
 import redis
 from urllib.parse import urljoin
-from config import REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASSWORD
+from config import REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASSWORD, api_headers, api_url
 from curl_cffi import requests
 from pyquery import PyQuery as pq
+import json_repair
+
 
 # 初始化 Redis 客户端
 redis_client = redis.Redis(
@@ -73,10 +75,57 @@ def scrape_huangli():
         }
         for tr in month_table.find("tr").items()
     ]
+    if data['lunar_calendar'] == "":
+        chat_get_calendar()
+    else:
+        json_result = json.dumps(data, ensure_ascii=False, indent=4)
+        redis_client.set("yellowCalendar", json_result)
 
-    # 保存到 Redis
-    json_result = json.dumps(data, ensure_ascii=False, indent=4)
-    redis_client.set("yellowCalendar", json_result)
+
+def chat_get_calendar():
+    data = redis_client.get("yellowCalendar")
+    need_chat = False
+    if data:
+        data = json.loads(data)
+        if "lunar_calendar" in data:
+            if data['lunar_calendar'] == "":
+                need_chat = True
+        else:
+            need_chat = True
+    else:
+        need_chat = True
+    if need_chat:
+        today = time.strftime("%Y-%m-%d", time.localtime())
+        res = requests.post(api_url, headers=api_headers, json={
+            "messages": [
+                {
+                  "role": "system",
+                  "content": "你是一个黄历助手，熟悉黄历计算，现在阳历日期是"+time.strftime("%Y-%m-%d", time.localtime())
+                },
+                {
+                    "role": "user",
+                    "content": "给我今天的黄历信息,lunar_calendar需要是天干地支纪年法 农历月日 生肖，需要包含以以下格式返回给我，返回一个json格式的数据："+json.dumps({"gregorian_calendar": "", "lunar_calendar": "", "good_actions": [], "bad_actions": []})
+                }
+            ],
+            "model": "deepseek-chat",
+            "response_format": {"type": "json_object"},
+            "max_tokens": 4096,
+            "stream": True
+        }, stream=True)
+        text = ""
+        for line in res.iter_lines():
+            if line:
+                line = line.decode("utf-8")
+                if line and line.startswith("data: ") and not line.endswith("[DONE]"):
+                    data = json.loads(line[len("data: "):])
+                    if "choices" in data:
+                        if data["choices"] and len(data["choices"]) > 0 and "delta" in data["choices"][0]:
+                            chunk = data["choices"][0]["delta"].get("content", "")
+                            text += chunk
+        data = json.loads(json_repair.repair_json(text))
+        json_result = json.dumps(data, ensure_ascii=False, indent=4)
+        redis_client.set("yellowCalendar", json_result)
+
 
 def scrape_datehoer():
     url = "https://www.datehoer.com/"
@@ -103,6 +152,7 @@ def scrape_datehoer():
     }
 
     redis_client.set("myblog", json.dumps(result, ensure_ascii=False))
+
 
 if __name__ == "__main__":
     scrape_huangli()
